b9805daca0f6d488ade3c755bed8ab1bd5e6411d
Pavel Ershov <pave1ershov@yandex.ru>
2023-07-15 19:10:35 +0300

    final result

--- Source.cpp
+++ Source.cpp
@@ -6,17 +6,31 @@
 #include <vector>
 #define PI 3.141592653589793
 
+struct Light {
+	Light(const vec3f& p, const float i) : position(p), intensity(i) {}
+	vec3f position;
+	float intensity;
+};
 
-struct sphere {
-private:
+struct Material {
+	vec3f diffuse_color;
+	vec4f albedo;
+	float specular_exponent;
+	float refractive_index;
+	Material(const float r, const vec4f& a, const vec3f& color, const float spec) : refractive_index(r), albedo(a), diffuse_color(color), specular_exponent(spec) {}
+	Material() : refractive_index(1), albedo(1, 0, 0, 0), diffuse_color(), specular_exponent() {}
+};
+
+struct Sphere {
 	vec3f center;
 	float radius;
-public:
-	sphere(const vec3f &c, const float &r) : center(c), radius(r) {}
+	Material material;
+
+	Sphere(const vec3f& c, const float r, const Material& m) : center(c), radius(r), material(m) {}
 
 	bool ray_intersect(const vec3f& orig, const vec3f& dir, float& t0) const {
-		
-		/*vec3f L = center - orig;
+
+		vec3f L = center - orig;
 		float tca = L * dir;
 		float d2 = L * L - tca * tca;
 		if (d2 > radius * radius) return false;
@@ -25,48 +39,103 @@ public:
 		float t1 = tca + thc;
 		if (t0 < 0) t0 = t1;
 		if (t0 < 0) return false;
-		return true;*/
-		
-		vec3f s = center - orig;
-		float ds = dir * s;
-		float d = 4 * ds * ds - 4 * (dir * dir) * (s * s - radius * radius);
-		if (d < 0) return false; 
 		return true;
 
 	}
 };
 
-vec3f cast_ray(const vec3f& orig, const vec3f& dir, const sphere& sphere) {
-
-	float sphere_dist = std::numeric_limits<float>::max();
-	if (!sphere.ray_intersect(orig, dir, sphere_dist)) {
-		return vec3f(1, 1, 1);
-	}
-	return vec3f(0, 0, 0);
+vec3f reflect(const vec3f& I, const vec3f& N) {
+	return I - N * 2.f * (I * N);
 }
 
-void render(const sphere& sph) {
-	const int width = 1024;
-	const int height = 768;
-	const double fov = PI / 2.;
-	std::vector<vec3f> framebuffer(width * height);
+vec3f refract(const vec3f& I, const vec3f& N, const float eta_t, const float eta_i = 1.f) {
+	float cosi = -std::max(-1.f, std::min(1.f, I * N));
+	if (cosi < 0) return refract(I, N * -1.f, eta_i, eta_t);
+	float eta = eta_i / eta_t;
+	float k = 1 - eta * eta * (1 - cosi * cosi);
+	return k < 0 ? vec3f(1, 0, 0) : I * eta + N * (eta * cosi - sqrtf(k));
+}
 
-	#pragma omp parallel for
-	for (size_t j = 0; j < height; j++) {
-		for (size_t i = 0; i < width; i++) {
-			float x = (2 * (i + 0.5) / (float)width - 1) * tan(fov / 2.) * width / (float)height;
-			float y = -(2 * (j + 0.5) / (float)height - 1) * tan(fov / 2.);
-			vec3f dir = vec3f(x, y, -1).normalize();
-			framebuffer[i + j * width] = cast_ray(vec3f(0, 0, 0), dir, sph);
-
-			//framebuffer[i + j * width] = vec3f(i/ float(width), 255, j / float(height));
+bool scene_intersect(const vec3f& orig, const vec3f& dir, const std::vector<Sphere>& spheres, vec3f& hit, vec3f& N, Material& material) {
+	float spheres_dist = std::numeric_limits<float>::max();
+	for (size_t i = 0; i < spheres.size(); i++) {
+		float dist_i;
+		if (spheres[i].ray_intersect(orig, dir, dist_i) && dist_i < spheres_dist) {
+			spheres_dist = dist_i;
+			hit = orig + dir * dist_i;
+			N = (hit - spheres[i].center).normalize();
+			material = spheres[i].material;
 		}
 	}
+	float checkerboard_dist = std::numeric_limits<float>::max();
+	if (fabs(dir[1]) > float(1e-3)) {
+		float d = -(orig[1] + 4) / dir[1];
+		vec3f pt = orig + dir * d;
+		if (d > 0 && fabs(pt[0]) < 15 && pt[2]<0 && pt[2]>-40 && d < spheres_dist) {
+			checkerboard_dist = d;
+			hit = pt;
+			N = vec3f(0, 1, 0);
+			material.diffuse_color = (int(.5 * hit[0] + 1000) + int(.5 * hit[2])) & 1 ? vec3f(1, 1, 1) : vec3f(0, 0, 0);
+			material.diffuse_color = material.diffuse_color * float(.3);
+		}
+	}
+	return std::min(spheres_dist, checkerboard_dist) < 1000;
+}
+
+vec3f cast_ray(const vec3f& orig, const vec3f& dir, const std::vector<Sphere>& spheres, const std::vector<Light>& lights, size_t depth = 0) {
+	vec3f point, N;
+	Material material;
+	if (depth > 4 || !scene_intersect(orig, dir, spheres, point, N, material)) {
+		return vec3f(0, 0, 0);
+	}
+
+	vec3f reflect_dir = reflect(dir, N);
+	vec3f refract_dir = refract(dir, N, material.refractive_index).normalize();
+	vec3f reflect_orig = reflect_dir * N < 0 ? point - N * float(1e-3) : point + N * float(1e-3);
+	vec3f refract_orig = refract_dir * N < 0 ? point - N * float(1e-3) : point + N * float(1e-3);
+	vec3f reflect_color = cast_ray(reflect_orig, reflect_dir, spheres, lights, depth + 1);
+	vec3f refract_color = cast_ray(refract_orig, refract_dir, spheres, lights, depth + 1);
+
+
+	float diffuse_light_intensity = 0, specular_light_intensity = 0;
+	for (size_t i = 0; i < lights.size(); i++) {
+		vec3f light_dir = (lights[i].position - point).normalize();
+
+		float light_distance = (lights[i].position - point).norm();
+		vec3f shadow_orig = light_dir * N < 0 ? point - N * float(1e-3) : point + N * float(1e-3);
+		vec3f shadow_pt, shadow_N;
+		Material tmpmaterial;
+		if (scene_intersect(shadow_orig, light_dir, spheres, shadow_pt, shadow_N, tmpmaterial) && (shadow_pt - shadow_orig).norm() < light_distance) continue;
+
+		diffuse_light_intensity += lights[i].intensity * std::max(0.f, light_dir * N);
+		specular_light_intensity += powf(std::max(0.f, reflect(light_dir, N) * dir), material.specular_exponent) * lights[i].intensity;
+	}
+	return material.diffuse_color * diffuse_light_intensity * material.albedo[0] + vec3f(1., 1., 1.) * specular_light_intensity * material.albedo[1] + reflect_color * material.albedo[2] + refract_color * material.albedo[3];
+}
+
+void render(const std::vector<Sphere>& spheres, const std::vector<Light>& lights) {
+	const int width = 1920;
+	const int height = 1080;
+	const double fov = PI / 2.;
+	std::vector<vec3f> framebuffer(width * height);
+#pragma omp parallel for
+	for (size_t j = 0; j < height; j++) {
+		for (size_t i = 0; i < width; i++) {
+			float dir_x = (i + 0.5) - width / 2.;
+			float dir_y = -(j + 0.5) + height / 2.;
+			float dir_z = -height / (2. * tan(fov / 2.));
+			framebuffer[i + j * width] = cast_ray(vec3f(0, 0, 0), vec3f(dir_x, dir_y, dir_z).normalize(), spheres, lights);
+		}
+	}
+
 
 	std::ofstream ofs;
 	ofs.open("spheres.ppm", std::ofstream::out | std::ofstream::binary);
 	ofs << "P6\n" << width << " " << height << "\n255\n";
 	for (size_t i = 0; i < height * width; ++i) {
+		vec3f& c = framebuffer[i];
+		float max = std::max(c[0], std::max(c[1], c[2]));
+		if (max > 1) c = c * float((1. / max));
 		for (size_t j = 0; j < 3; j++) {
 			ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
 		}
@@ -76,8 +145,27 @@ void render(const sphere& sph) {
 
 int main() {
 
-	sphere sph(vec3f(-3, 0, -16), 2);
-	render(sph);
-	
+	Material carbon(1.0, vec4f(0.6, 0.3, 0.0, 0.0), vec3f(0.188, 0.188, 0.188), 50.);
+	Material aluminium(1.0, vec4f(0.9, 0.1, 0.1, 0.0), vec3f(0.8, 0.8, 0.8), 10.);
+	Material mirror(1.0, vec4f(0.0, 10.0, 0.8, 0.0), vec3f(1.0, 1.0, 1.0), 1425.);
+	Material glass(1.5, vec4f(0.0, 0.5, 0.1, 0.8), vec3f(0.6, 0.7, 0.8), 125.);
+
+	std::vector<Sphere> spheres;
+	spheres.push_back(Sphere(vec3f(-3, -0, -16), 2, carbon));
+	spheres.push_back(Sphere(vec3f(-1.0, -1.5, -12), 2, glass));
+	spheres.push_back(Sphere(vec3f(1.5, -0.5, -18), 3, carbon));
+	spheres.push_back(Sphere(vec3f(7, 5, -18), 4, aluminium));
+	spheres.push_back(Sphere(vec3f(-6, 10, -20), 7, mirror));
+	spheres.push_back(Sphere(vec3f(3, 4, -8), 1, aluminium));
+	spheres.push_back(Sphere(vec3f(-2, -5, -7), 0.6, carbon));
+	spheres.push_back(Sphere(vec3f(-3, -6, -10), 2, glass));
+	std::vector<Light>  lights;
+	lights.push_back(Light(vec3f(-20, 20, 20), 1.5));
+	lights.push_back(Light(vec3f(30, 50, -25), 1.8));
+	lights.push_back(Light(vec3f(30, 20, 30), 1.7));
+
+	render(spheres, lights);
+
+
 	return 0;
 }
--- vec.h
+++ vec.h
@@ -9,7 +9,7 @@ private:
 	T data[dim];
 public:
 
-	T &operator [](const size_t i) {
+	T& operator [](const size_t i) {
 		try {
 			if (i > dim) {
 				throw std::invalid_argument("Ошибка!!! Зашли за границу массива!");
@@ -21,7 +21,7 @@ public:
 		return data[i];
 	}
 
-	const T &operator [](const size_t i) const {
+	const T& operator [](const size_t i) const {
 		try {
 			if (i > dim) {
 				throw std::invalid_argument("Ошибка!!! Зашли за границу массива!");
@@ -43,23 +43,23 @@ public:
 		return std::sqrt((*this) * (*this));
 	}
 
-	vec<dim, T> normalize() const{
+	vec<dim, T> normalize() const {
 		return (*this) / norm();
 	}
 };
 
-template<size_t dim, typename T> T operator *(const vec<dim, T> &lv, const vec<dim, T>&pv) {
+template<size_t dim, typename T> T operator *(const vec<dim, T>& lv, const vec<dim, T>& pv) {
 	T res = 0;
 	for (size_t i = dim; i--; res += lv[i] * pv[i]);
 	return res;
 }
 
-template<size_t dim, typename T>vec<dim, T> operator +(vec<dim, T> lv, const vec<dim, T>&pv) {
+template<size_t dim, typename T>vec<dim, T> operator +(vec<dim, T> lv, const vec<dim, T>& pv) {
 	for (size_t i = dim; i--; lv[i] += pv[i]);
 	return lv;
 }
 
-template<size_t dim, typename T>vec<dim, T> operator -(vec<dim, T> lv, const vec<dim, T>&pv) {
+template<size_t dim, typename T>vec<dim, T> operator -(vec<dim, T> lv, const vec<dim, T>& pv) {
 	for (size_t i = dim; i--; lv[i] -= pv[i]);
 	return lv;
 }
@@ -74,7 +74,7 @@ template<size_t dim, typename T>vec<dim, T> operator /(vec<dim, T> lv, const T n
 	return lv;
 }
 
-template<size_t len, size_t dim, typename T> vec<len, T> embed(const vec<dim, T> &v, const T &fill = 1 ) {
+template<size_t len, size_t dim, typename T> vec<len, T> embed(const vec<dim, T>& v, const T& fill = 1) {
 	vec<len, T> res = vec<len, T>::fill(fill);
 	for (size_t i = dim; i--; res[i] = v[i]);
 	return res;
@@ -109,23 +109,6 @@ typedef vec<3, float> vec3f;
 typedef vec<3, int> vec3i;
 typedef vec<4, float> vec4f;
 
-//template <typename T> struct vec<2, T> {
-//	vec() : x(T()), y(T()) {}
-//	vec(T X, T Y) : x(X), y(Y) {}
-//
-//	T& operator [](const size_t i) {
-//		assert(i < 2);
-//		return i <= 0 ? x : y;
-//	}
-//
-//	const T& operator [](const size_t i) const {
-//		assert(i < 2);
-//		return i <= 0 ? x : y;
-//	}
-//private:
-//	T x, y;
-//};
-//
 template <typename T> struct vec<3, T> {
 	vec() : x(T()), y(T()), z(T()) {}
 	vec(T X, T Y, T Z) : x(X), y(Y), z(Z) {}
@@ -139,7 +122,7 @@ template <typename T> struct vec<3, T> {
 		catch (std::exception const& ex) {
 			std::cout << ex.what() << std::endl;
 		}
-		return i <= 0 ? x : (1==i? y : z);
+		return i <= 0 ? x : (1 == i ? y : z);
 	}
 
 	const T& operator [](const size_t i) const {
@@ -165,20 +148,83 @@ template <typename T> struct vec<3, T> {
 private:
 	T x, y, z;
 };
-//
-//template <typename T> struct vec<4, T> {
-//	vec() : x(T()), y(T()), z(T()), w(T()) {}
-//	vec(T X, T Y, T Z, T W) : x(X), y(Y), z(Z), w(W) {}
-//
-//	T& operator[](const size_t i) { 
-//		assert(i < 4);
-//		return i <= 0 ? x : (1 == i ? y : (2 == i ? z : w)); 
-//	}
-//
-//	const T& operator[](const size_t i) const { 
-//		assert(i < 4); 
-//		return i <= 0 ? x : (1 == i ? y : (2 == i ? z : w)); 
-//	}
-//private:
-//	T x, y, z, w;
-//};
+
+template <typename T> struct vec<2, T> {
+	vec() : x(T()), y(T()) {}
+	vec(T X, T Y) : x(X), y(Y) {}
+
+	T& operator [](const size_t i) {
+		try {
+			if (i > 2) {
+				throw std::invalid_argument("Ошибка!!! Зашли за границу массива!");
+			}
+		}
+		catch (std::exception const& ex) {
+			std::cout << ex.what() << std::endl;
+		}
+		return i <= 0 ? x : y;
+	}
+
+	const T& operator [](const size_t i) const {
+		try {
+			if (i > 2) {
+				throw std::invalid_argument("Ошибка!!! Зашли за границу массива!");
+			}
+		}
+		catch (std::exception const& ex) {
+			std::cout << ex.what() << std::endl;
+		}
+		return i <= 0 ? x : y;
+	}
+
+	T norm() const {
+		return std::sqrt((*this) * (*this));
+	}
+
+	vec<2, T> normalize() const {
+		return (*this) / norm();
+	}
+
+private:
+	T x, y;
+};
+
+template <typename T> struct vec<4, T> {
+	vec() : x(T()), y(T()), z(T()), w(T()) {}
+	vec(T X, T Y, T Z, T W) : x(X), y(Y), z(Z), w(W) {}
+
+	T& operator [](const size_t i) {
+		try {
+			if (i > 4) {
+				throw std::invalid_argument("Ошибка!!! Зашли за границу массива!");
+			}
+		}
+		catch (std::exception const& ex) {
+			std::cout << ex.what() << std::endl;
+		}
+		return i <= 0 ? x : (1 == i ? y : (2 == i ? z : w));
+	}
+
+	const T& operator [](const size_t i) const {
+		try {
+			if (i > 4) {
+				throw std::invalid_argument("Ошибка!!! Зашли за границу массива!");
+			}
+		}
+		catch (std::exception const& ex) {
+			std::cout << ex.what() << std::endl;
+		}
+		return i <= 0 ? x : (1 == i ? y : (2 == i ? z : w));
+	}
+
+	T norm() const {
+		return std::sqrt((*this) * (*this));
+	}
+
+	vec<3, T> normalize() const {
+		return (*this) / norm();
+	}
+
+private:
+	T x, y, z, w;
+};
